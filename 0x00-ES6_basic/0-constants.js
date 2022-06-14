31d278882b8d045808c22afe57f95dcee8e210d7
kalom60 <39136564+kalom60@users.noreply.github.com>
2022-06-14 15:28:08 +0300

    modify variables inside the functions

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,15 +1,15 @@
-export const taskFirst = () => {
-    var task = 'I prefer const when I can.'
+export function taskFirst() {
+    const task = 'I prefer const when I can.';
     return task;
 }
 
 export function getLast() {
-    return ' is okay.'
+    return ' is okay';
 }
 
-export let taskLast = () => {
-    var combination = 'But sometimes let'
-    combination += getLast()
+export function taskNext() {
+    let combination = 'But sometimes let';
+    combination += getLast();
 
-    return combination
+    return combination;
 }
