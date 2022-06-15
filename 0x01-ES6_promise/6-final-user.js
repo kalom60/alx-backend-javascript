43e57530b89bc8288ae52f34fa9a9c8d8e8f2556
kalom60 <39136564+kalom60@users.noreply.github.com>
2022-06-16 00:01:00 +0300

    fixing my imported module name

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,11 +1,11 @@
-import signUser from './4-user-promise';
+import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise
-    .allSettled([signUser(firstName, lastName), uploadPhoto(fileName)])
+    .allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
     .then((res) => {
-      res.forEach((r) => ({
+      res.map((r) => ({
         status: r.status,
         value: r.value === 'fillfulled' ? r.value : String(r.reason),
       }));
