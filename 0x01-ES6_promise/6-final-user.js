d8dcd451c1ced30c61eaaa929ed213c45678df3a
kalom60 <39136564+kalom60@users.noreply.github.com>
2022-06-16 00:04:04 +0300

    using async and .map to loop thru res

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,13 +1,13 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstName, lastName, fileName) {
+export default async function handleProfileSignup(firstName, lastName, fileName) {
   return Promise
     .allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((res) => {
-      res.map((r) => ({
-        status: r.status,
-        value: r.value === 'fillfulled' ? r.value : String(r.reason),
-      }));
-    });
+    .then((res) => (
+      res.map((o) => ({
+        status: o.status,
+        value: o.status === 'fulfilled' ? o.value : String(o.reason),
+      }))
+    ));
 }
