e785749fde09df7da707e844415fb99e5f532ac1
kalom60 <39136564+kalom60@users.noreply.github.com>
2022-06-21 20:14:09 +0300

    fix error

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -9,12 +9,9 @@ const cleanSet = (set, startString) => {
   }
 
   const newString = [];
-  for (const str of set.values()) {
-    if (str.startsWith(startString)) {
-      const val = str.substring(startString.length);
-      if (val && val !== str) {
-        newString.push(val);
-      }
+  for (const val of set) {
+    if (val && val.startsWith(startString)) {
+      newString.push(val.slice(startString.length));
     }
   }
   return newString.join('-');
