e04c408d0dc7638444b03d21548fbaa43289c985
kalom60 <39136564+kalom60@users.noreply.github.com>
2022-08-09 17:24:25 +0300

    fsPromise

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -1,8 +1,10 @@
-const fs = require('fs/promises');
+const fs = require('fs');
+
+const fsPromise = fs.promises;
 
 const countStudents = async (dataPath) => {
-  try {
-    let data = await fs.readFile(dataPath);
+  if (fs.existsSync(dataPath)) {
+    let data = await fsPromise.readFile(dataPath);
     data = data.toString();
     const lines = data.split('\n').slice(1);
     const cs = [];
@@ -23,8 +25,6 @@ const countStudents = async (dataPath) => {
     const sweNames = swe.join(', ');
     console.log(`Number of students in CS: ${cs.length}. List: ${csNames}`);
     console.log(`Number of students in SWE: ${swe.length}. List: ${sweNames}`);
-  } catch (err) {
-    throw new Error('Cannot load the database');
   }
 };
 
