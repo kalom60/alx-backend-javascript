2560f7c58febd9cab7f2828c2d5e38007f962f31
kalom60 <39136564+kalom60@users.noreply.github.com>
2022-08-09 22:17:52 +0300

    express with fsPromise

--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const fs = require('fs');
 
+const fsPromise = fs.promises;
 const app = express();
 const port = 1245;
 
@@ -11,35 +12,34 @@ app.get('/', (req, res) => {
 });
 
 app.get('/students', (req, res) => {
-  const msg = 'This is the list of our students\n';
-  try {
-    fs.readFile(db, 'utf-8', (err, data) => {
-      if (data) {
-        const lines = data.split('\n').slice(1);
+  if (fs.existsSync(db)) {
+    fsPromise.readFile(db)
+      .then((data) => {
+        const lines = data.toString().split('\n');
+        let response = lines.filter((item) => item);
+        response = response.map((item) => item.split(','));
+
         const cs = [];
         const swe = [];
-        for (const student of lines) {
-          if (student.includes('CS')) {
-            let name = student.split(',').slice(0, 1);
-            name = String(name);
-            cs.push(name);
-          } else {
-            let name = student.split(',').slice(0, 1);
-            name = String(name);
-            swe.push(name);
+        for (const i of response) {
+          for (let j = 0; j < i.length; j += 1) {
+            if (i[j] === 'CS') {
+              cs.push(i[0]);
+            }
+            if (i[j] === 'SWE') {
+              swe.push(i[0]);
+            }
           }
         }
         const displayLine = [];
-        displayLine.push(`Number of students: ${lines.length}`);
-        // const csNames = cs.join(', ');
-        // const sweNames = swe.join(', ');
+        displayLine.push('This is the list of our students');
+        displayLine.push(`Number of students: ${response.length - 1}`);
         displayLine.push(`Number of students in CS: ${cs.length}. List: ${cs.join(', ')}`);
         displayLine.push(`Number of students in SWE: ${swe.length}. List: ${swe.join(', ')}`);
-        res.status(200).send(displayLine.join('\n'));
-      }
-    });
-  } catch (err) {
-    res.send(`${msg}${err.message}`);
+        res.send(`${displayLine.join('\n')}`);
+      });
+  } else {
+    throw new Error('Cannot load the database');
   }
 });
 
