8c4db890701f239bf09eca1247176cf283503385
kalom60 <39136564+kalom60@users.noreply.github.com>
2022-08-09 17:37:04 +0300

    returning promise

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -1,33 +1,33 @@
 const fs = require('fs');
 
-const fsPromise = fs.promises;
-
-const countStudents = async (dataPath) => {
-  if (fs.existsSync(dataPath)) {
-    let data = await fsPromise.readFile(dataPath);
-    data = data.toString();
-    const lines = data.split('\n').slice(1);
-    const cs = [];
-    const swe = [];
-    for (const student of lines) {
-      if (student.includes('CS')) {
-        let name = student.split(',').slice(0, 1);
-        name = String(name);
-        cs.push(name);
-      } else {
-        let name = student.split(',').slice(0, 1);
-        name = String(name);
-        swe.push(name);
+const countStudents = async (dataPath) => { //eslint-disable-line
+  return new Promise((resolve, reject) => {
+    fs.readFile(dataPath, 'utf-8', (err, data) => {
+      if (err) {
+        reject(Error('Cannot load the database'));
       }
-    }
-    console.log(`Number of students: ${lines.length}`);
-    const csNames = cs.join(', ');
-    const sweNames = swe.join(', ');
-    console.log(`Number of students in CS: ${cs.length}. List: ${csNames}`);
-    console.log(`Number of students in SWE: ${swe.length}. List: ${sweNames}`);
-  } else {
-    throw new Error('Cannot load the database');
-  }
+      const lines = data.split('\n').slice(1);
+      const cs = [];
+      const swe = [];
+      for (const student of lines) {
+        if (student.includes('CS')) {
+          let name = student.split(',').slice(0, 1);
+          name = String(name);
+          cs.push(name);
+        } else {
+          let name = student.split(',').slice(0, 1);
+          name = String(name);
+          swe.push(name);
+        }
+      }
+      console.log(`Number of students: ${lines.length}`);
+      console.log(`Number of students in CS: ${cs.length}. List: ${cs.join(', ')}`);
+      console.log(
+        `Number of students in SWE: ${swe.length}. List: ${swe.join(', ')}` //eslint-disable-line
+      );
+      resolve(lines);
+    });
+  });
 };
 
 module.exports = countStudents;
