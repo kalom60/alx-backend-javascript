e6efba0376e087e896f02f82780b1c34ca3b87c8
kalom60 <39136564+kalom60@users.noreply.github.com>
2022-08-09 23:08:28 +0300

    use stdin

--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -8,53 +8,44 @@ const host = 'localhost';
 
 const app = express();
 
-async function countStudents(dataPath) {
-  if (fs.existsSync(dataPath)) {
-    const data = await fsPromise.readFile(dataPath);
-    const lines = data.toString().split('\n');
-    let res = lines.filter((item) => item);
-    res = res.map((item) => item.split(','));
-
-    const cs = [];
-    const swe = [];
-    for (const i of res) {
-      for (let j = 0; j < i.length; j += 1) {
-        if (i[j] === 'CS') {
-          cs.push(i[0]);
-        }
-        if (i[j] === 'SWE') {
-          swe.push(i[0]);
-        }
-      }
-    }
-    const send = [];
-    send.push(`Number of students: ${res.length - 1}`);
-    send.push(`Number of students in CS: ${cs.length}. List: ${cs.join(', ')}`);
-    send.push(`Number of students in SWE: ${swe.length}. List: ${swe.join(', ')}`);
-    return send;
-  }
-  return 'This is the list of our students';
-}
-
 app.get('/', (req, res) => {
   res.send('Hello Holberton School!');
 });
 
 app.get('/students', (req, res) => {
-  countStudents(db)
-    .then((data) => {
-      try {
-        res.setHeader('Content-Type', 'text/plain');
-        res.statusCode = 200;
-        res.send(`${data.join('\n')}`);
-      } catch (err) {
-        res.send(data);
-      }
-    });
+  if (fs.existsSync(db)) {
+    fsPromise.readFile(db)
+      .then((data) => {
+        const lines = data.toString().split('\n');
+        let response = lines.filter((item) => item);
+        response = response.map((item) => item.split(','));
+
+        const cs = [];
+        const swe = [];
+        for (const i of response) {
+          for (let j = 0; j < i.length; j += 1) {
+            if (i[j] === 'CS') {
+              cs.push(i[0]);
+            }
+            if (i[j] === 'SWE') {
+              swe.push(i[0]);
+            }
+          }
+        }
+        const displayLine = [];
+        displayLine.push('This is the list of our students');
+        displayLine.push(`Number of students: ${response.length - 1}`);
+        displayLine.push(`Number of students in CS: ${cs.length}. List: ${cs.join(', ')}`);
+        displayLine.push(`Number of students in SWE: ${swe.length}. List: ${swe.join(', ')}`);
+        res.send(`${displayLine.join('\n')}`);
+      });
+  } else {
+    res.send('This is the list of our students');
+  }
 });
 
 app.listen(port, host, () => {
-  // console.log(`Example app listening on port ${port}`);
+  process.stdin.write(`app listening on port ${port}`);
 });
 
 module.exports = app;
