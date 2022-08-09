559741966e4ee125ff8179aee259f740db4ae26e
kalom60 <39136564+kalom60@users.noreply.github.com>
2022-08-10 02:40:41 +0300

    calling 3-read_file_async file

--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const fs = require('fs');
 
 const port = 1245;
 const db = process.argv[2];
@@ -7,50 +6,25 @@ const host = 'localhost';
 
 const app = express();
 
-function countStudents(path) {
-  return new Promise((resolve, reject) => {
-    fs.readFile(path, 'utf8', (err, data) => {
-      if (err) {
-        reject(Error('This is the list of our students'));
-        return;
-      }
-      const lines = data.toString().split('\n');
-      let response = lines.filter((item) => item);
-      response = response.map((item) => item.split(','));
-
-      const cs = [];
-      const swe = [];
-      for (const i of response) {
-        for (let j = 0; j < i.length; j += 1) {
-          if (i[j] === 'CS') {
-            cs.push(i[0]);
-          }
-          if (i[j] === 'SWE') {
-            swe.push(i[0]);
-          }
-        }
-      }
-      const displayLine = [];
-      displayLine.push('This is the list of our students');
-      displayLine.push(`Number of students: ${response.length - 1}`);
-      displayLine.push(`Number of students in CS: ${cs.length}. List: ${cs.join(', ')}`);
-      displayLine.push(`Number of students in SWE: ${swe.length}. List: ${swe.join(', ')}`);
-      resolve(displayLine);
-    });
-  });
-}
+const countStudents = require('./3-read_file_async');
 
 app.get('/', (req, res) => {
+  res.setHeader('Content-Type', 'text/plain');
   res.send('Hello Holberton School!');
 });
 
 app.get('/students', async (req, res) => {
-  try {
-    const students = await countStudents(db);
-    res.send(`${students.join('\n')}`);
-  } catch (error) {
-    res.send(`${error.message}`);
-  }
+  res.setHeader('Content-Type', 'text/plain');
+  res.write('This is the list of our students\n');
+
+  await countStudents(db).then((data) => {
+    res.write(`Number of students: ${data.res.length - 1}\n`);
+    res.write(`Number of students in CS: ${data.cs.length}. List: ${data.cs.join(', ')}\n`);
+    res.write(`Number of students in SWE: ${data.swe.length}. List: ${data.swe.join(', ')}`);
+  }).catch((err) => res.write(err.message))
+    .finally(() => {
+      res.end();
+    });
 });
 
 app.listen(port, host, () => {
