4e61ed2b28849c20bad3d97b304aaeda381154b3
kalom60 <39136564+kalom60@users.noreply.github.com>
2022-08-11 13:42:08 +0300

    Hooks

---
+++ 0x06-unittests_in_js/5-payment.test.js
@@ -0,0 +1,28 @@
+const sinon = require('sinon');
+const chai = require('chai');
+const expect = chai.expect;
+
+const sendPaymentRequestToApi = require('./3-payment');
+const Utils = require('./utils');
+
+describe('', () => {
+  let spy;
+  beforeEach(() => {
+    spy = sinon.spy(console, 'log');
+  });
+
+  afterEach(() => {
+    sinon.restore();
+  });
+  it('', () => {
+    sendPaymentRequestToApi(100, 20);
+    expect(console.log.calledWith('The total is: 120')).to.be.true;
+    expect(spy.calledOnce).to.be.true;
+  });
+
+  it('', () => {
+    sendPaymentRequestToApi(10, 10);
+    expect(console.log.calledWith('The total is: 20')).to.be.true;
+    expect(spy.calledOnce).to.be.true;
+  });
+});
