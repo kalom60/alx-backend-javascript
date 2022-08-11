7b628b7ffc009001ed920f54f497fb97f2f144bf
kalom60 <39136564+kalom60@users.noreply.github.com>
2022-08-11 11:49:06 +0300

    add one more test

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,7 +1,7 @@
 const assert = require('assert');
 const calculateNumber = require('./0-calcul');
 
-describe('calculateNumber()', () => {
+describe('SUM', () => {
   it('should return 4 after adding 1, and 3', () => {
     assert.equal(calculateNumber(1, 3), 4);
   });
@@ -13,5 +13,8 @@ describe('calculateNumber()', () => {
   });
   it('should return 6 after adding 1.5, and 3.7', () => {
     assert.equal(calculateNumber(1.5, 3.7), 6);
+  });
+  it('should return 10 after adding 5.8, and 4,4', () => {
+    assert.equal(calculateNumber(5.8, 4.4), 10);
   });
 });
