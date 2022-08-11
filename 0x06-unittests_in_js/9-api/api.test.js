30ddd75cc4c73260e57d6e0c237448b7045b677d
kalom60 <39136564+kalom60@users.noreply.github.com>
2022-08-11 17:11:33 +0300

    all in one suite

--- 0x06-unittests_in_js/9-api/api.test.js
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -1,20 +1,16 @@
 const request = require('request');
 const { expect } = require('chai');
 
-describe('Index page', () => {
+describe('Api test', () => {
   const url = 'http://localhost:7865';
 
-  it('index page', (done) => {
+  it('Index page', (done) => {
     request.get(url, (err, res, data) => {
       expect(res.statusCode).to.equal(200);
       expect(data).to.equal('Welcome to the payment system');
       done();
     });
   });
-});
-
-describe('Cart Page', () => {
-  const url = 'http://localhost:7865';
 
   it('Check cart page with id which is number', (done) => {
     request.get(`${url}/cart/12`, (err, res, data) => {
