bc15e6184f6c6870aea98f2725935ef1d71dd098
kalom60 <39136564+kalom60@users.noreply.github.com>
2022-08-11 11:57:01 +0300

    more tests

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -17,4 +17,10 @@ describe('SUM', () => {
   it('should return 10 after adding 5.8, and 4,4', () => {
     assert.equal(calculateNumber(5.8, 4.4), 10);
   });
+  it('should return 22 after adding -15.3, and 36.7', () => {
+    assert.equal(calculateNumber(-15.3, 36.7), 22);
+  });
+  it('should return -7 after adding -4.4, and -3.3', () => {
+    assert.equal(calculateNumber(-4.4, -3.3), -7);
+  });
 });
